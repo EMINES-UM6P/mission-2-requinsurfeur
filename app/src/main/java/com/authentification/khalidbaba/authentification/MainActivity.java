41deb532e5cb942ddbba66702b90da6e45aa0af0
adam.elgorch <LoujTaud9oshpAm>
2018-01-29 18:25:12 +0000

    All Clear...

--- app/src/main/java/com/authentification/khalidbaba/authentification/MainActivity.java
+++ app/src/main/java/com/authentification/khalidbaba/authentification/MainActivity.java
@@ -1,10 +1,13 @@
 package com.authentification.khalidbaba.authentification;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by khalidbaba on 28/01/2018.
@@ -12,7 +15,9 @@ import android.widget.Button;
 
 
 public class MainActivity extends Activity {
-
+EditText Email;
+EditText Password;
+Button Btnlogin;
 
     // TODO : declare all variables (elements AND variables)
     @Override
@@ -21,11 +26,22 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_login);
 
         //TODO Instantiate object (EditText and Button)
+        Email = (EditText) findViewById(R.id.email);
+        Password = (EditText) findViewById(R.id.password);
+        Btnlogin = (Button) findViewById(R.id.btnLogin);
 
 
 
         //TODO : handle click listener in button and get values from the editText
-
+Btnlogin.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Log.i("btnregister", "button register cliqued");
+        Intent i = new Intent(MainActivity.this, WolcomeActivity.class);
+        i.putExtra("value", Email.getText().toString());
+        startActivity(i);
+    }
+});
 
         //TODO : Create Intent and send email content to the welcome_activity
 
